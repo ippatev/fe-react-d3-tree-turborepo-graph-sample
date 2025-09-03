c9cb8226d8860fde145fdfd6d44bb6888190b773
ippatev <alexanderippatiev24@gmail.com>
2025-09-03 18:03:12 +0900

    feat: add tree data

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,125 @@
 import Tree from "react-d3-tree";
 import "./app.css";
 
-const treeData = {};
+const treeData = {
+  name: "lk-general#build",
+  attributes: { type: "aggregator" },
+  children: [
+    {
+      name: "@repo/budget-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/finance-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/mero-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/np-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/finance-module#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/oks-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/oper-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/operinfo-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/rating-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/rivers-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/ship-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/team-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/vdl-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/vp-mero-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/mero-module#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+    {
+      name: "@repo/vp-module#build",
+      children: [
+        { name: "@repo/eslint-config#build" },
+        { name: "@repo/tailwind-config#build" },
+        { name: "@repo/typescript-config#build" },
+      ],
+    },
+  ],
+};
 
 export default function InteractiveTree() {
   return (
